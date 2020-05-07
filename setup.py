c9adb9fe4fa1a64d0e515680ec171589149835b1
vincentsarago <vincent.sarago@gmail.com>
2020-05-07 18:39:15 -0400

    release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extra_reqs = {
 
 setup(
     name="rio-viz",
-    version="0.1.0",
+    version="0.2.0",
     python_requires=">=3",
     description=u"Visualize Cloud Optimized GeoTIFF in browser",
     classifiers=[
