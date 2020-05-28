b130253aa0f2c3317f9800bc5dfd5e65594d9f03
Vincent Sarago <vincent.sarago@gmail.com>
2020-05-27 22:50:54 -0400

    [WIP] add rio-color formula to the `simple` UI (#16)
    * add rio-color formula to the `simple` UI
    
    * update for rio-tiler 2.0a9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 inst_reqs = [
     "click",
     "rio-cogeo",
-    "rio-tiler~=2.0a4",
+    "rio-tiler~=2.0a9",
     "rio-color",
     "fastapi",
     "uvicorn",
@@ -21,7 +21,7 @@ extra_reqs = {
 
 setup(
     name="rio-viz",
-    version="0.2.0",
+    version="0.2.1",
     python_requires=">=3",
     description=u"Visualize Cloud Optimized GeoTIFF in browser",
     classifiers=[
