df32f21ad6f545a174f8eb0a3a5dbc97d53a45bd
vincentsarago <vincent.sarago@gmail.com>
2021-01-27 10:14:32 -0500

    update version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ extra_reqs = {
 
 setup(
     name="rio-viz",
-    version="0.4.3.post1",
+    version="0.4.4",
     python_requires=">=3.6",
     description=u"Visualize Cloud Optimized GeoTIFF in browser",
     long_description=long_description,
