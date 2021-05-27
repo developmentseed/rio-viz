d5871567ae8df62ec9f184f018b0ef1c45adda4b
vincentsarago <vincent.sarago@gmail.com>
2021-05-26 22:00:09 -0400

    remove python 3.9

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: GIS",
     ],
     keywords="COG COGEO Rasterio GIS MVT",
