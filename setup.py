64e7ed3df16543bc8d7a0372b26fa29ef7f98f2b
vincentsarago <vincent.sarago@gmail.com>
2021-07-16 21:36:47 +0200

    Bump version: 0.6.1 → 0.7.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extra_reqs = {
 
 setup(
     name="rio-viz",
-    version="0.6.1",
+    version="0.7.0",
     python_requires=">=3.6",
     description=u"Visualize Cloud Optimized GeoTIFF in browser",
     long_description=long_description,
